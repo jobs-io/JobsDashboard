fc891210df989b8cc451dd76114498b5b1eb0b55
Adrian Ndwiga <adrian_ndwiga@hotmail.com>
2021-07-02 11:04:20 +1200

    adding console application

---
+++ src/JobsDashboard.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JobsDashboard.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
