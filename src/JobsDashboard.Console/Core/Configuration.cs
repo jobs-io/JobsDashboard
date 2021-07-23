5ebe25bf1f92647d3e98e53dff7b50913f1e26b5
Adrian Ndwiga <adrian_ndwiga@hotmail.com>
2021-07-23 18:30:11 +1200

    implementing configuration

---
+++ src/JobsDashboard.Console/Core/Configuration.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+
+namespace JobsDashboard.Console.Core
+{
+    public class Configuration : IConfiguration
+    {
+        private readonly IDictionary<string, string> items;
+
+        public Configuration(IDictionary<string, string> items)
+        {
+            this.items = items;   
+        }
+        public string GetValue(string key)
+        {
+            return items[key];
+        }
+    }
+}
