3baeb8d7def017c73d0c56e1756e129558aa4368
Adrian Ndwiga <adrian_ndwiga@hotmail.com>
2021-07-24 15:55:44 +1200

    implementing create jobs data store

--- src/JobsDashboard.Console/Data/DataStore.cs
+++ src/JobsDashboard.Console/Data/DataStore.cs
@@ -1,23 +1,28 @@
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using JobsDashboard.Data;
+using Newtonsoft.Json;
 
-namespace JobsDashboard.Console.Data
-{
-    public class DataStore : IDataStore
-    {
-        public void CreateJobs(IDictionary<string, string> data)
-        {
-            throw new System.NotImplementedException();
+namespace JobsDashboard.Console.Data {
+    public class DataStore : IDataStore {
+        private IDictionary<string, string> data;
+        private readonly string filePath;
+        public DataStore (string filePath) {
+            this.filePath = filePath;
+            this.data = JsonConvert.DeserializeObject<IDictionary<string, string>> (File.ReadAllText (filePath));
+        }
+        public void CreateJobs (IDictionary<string, string> data) {
+            this.data.Add(data.Keys.First(), data.Values.First());
+            File.WriteAllText(filePath, JsonConvert.SerializeObject(this.data));
         }
 
-        public bool Exists(string source)
-        {
-            throw new System.NotImplementedException();
+        public bool Exists (string source) {
+            throw new System.NotImplementedException ();
         }
 
-        public string GetJobs(string source)
-        {
-            throw new System.NotImplementedException();
+        public string GetJobs (string source) {
+            throw new System.NotImplementedException ();
         }
     }
 }
