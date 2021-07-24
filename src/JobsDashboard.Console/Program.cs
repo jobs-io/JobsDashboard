d8a68edb78cad16060cbabd6970c1b4ecebadba6
Adrian Ndwiga <adrian_ndwiga@hotmail.com>
2021-07-24 14:40:08 +1200

    read data store and source from passed arguments

--- src/JobsDashboard.Console/Program.cs
+++ src/JobsDashboard.Console/Program.cs
@@ -1,13 +1,29 @@
 ﻿using System;
+using System.IO;
 
 namespace JobsDashboard.Console
 {
+    public class Config {
+        public readonly string dataSource;
+        public readonly string source;
+
+        public Config(string[] args)
+        {
+            foreach(var arg in args) {
+                if (arg.StartsWith("-ds:"))
+                    this.dataSource = arg.Substring(4);
+                if(arg.StartsWith("-s:"))
+                    this.source = arg.Substring(3);
+            }   
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            foreach(var arg in args)
-                System.Console.WriteLine(arg);
+            var config = new Config(args);
+            System.Console.WriteLine(config.dataSource);
+            System.Console.WriteLine(config.source);
         }
     }
 }
